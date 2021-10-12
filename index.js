907ea7999e46e36f29df331ef89ae355964695dd
nnajera95 <NathanNajera@yahoo.com>
2021-10-12 17:03:05 -0500

    cleaned up and added code
    if applied this commit will rearrange some code to be in better order. It will also add express code for the endpoint /movies/[title].
    
    made table for endpoints
    
    if applied this commit will make a table detailing endpoints for the api.
    
    added express code
    
    if applied this commit will add express code for the endpoint /movie/[genre]
    
    added express code
    
    if applied this commit will add express code for the endpoint /movies/[director]
    
    added code to register and delete accounts
    
    if applied this commit will add middleware that allows a person to register for an account as well as deleting that account.
    
    added middleware and updated code
    
    if applied this commit will add middleware for updating an accounts username, along with updating code to better reflect its purpose in the app.delete middleware
    
    middleware added
    
    if applied this commit will add middleware that allows the user to add a movie to their favorites
    
    middleware added - remove
    
    if applied this commit will add middleware that will remove a movie from the user's favorites list.
    
    updated punctuation
    
    if applied this commit will update the punctuation in the middleware for getting the director
    
    updated errors in code
    
    if applied this commit will update errors found in the middleware for title, genre, and director

--- index.js
+++ index.js
@@ -20,12 +20,91 @@ app.use(morgan('common'));
 
 app.use(express.static('public'));
 
+app.get('/', function(_req, res) {
+  res.send('Welcome to my app!');
+});
+
 app.get('/movies', function(_req, res) {
   res.json(movieArray)
 });
 
-app.get('/', function(_req, res) {
-  res.send('Welcome to my app!');
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find(movie => {
+    return movie.title === req.params.title
+  }));
+});
+
+app.get('/movies/:genre', (req, res) => {
+  res.json(movies.find(movie => {
+    return movie.genre === req.params.genre
+  }));
+});
+
+app.get('/movies/:director', (req, res) => {
+  res.json(movies.find(movie => {
+    return movie.director === req.params.director
+  }));
+});
+
+app.post('/register', (req, res) => {
+  let newAccount = req.body;
+
+  if (!newAccount.username) {
+    const message = 'missing title in request body';
+    res.status(400).send(newAccount);
+  } else {
+      newUser.id = uuid.v4();
+      user.push(newAccount);
+      res.status(201).send(newAccount);
+  }
+});
+
+app.put('/account/:information/:update', (req, res) => {
+  let account = account.find((user) => {
+    return user.name === req.params.name
+  });
+
+  if (user) {
+    user.information[req.params.information] = parseInt(req.params.update);
+    res.status(201).send('account information ' + req.params.name + ' username was updated.')
+  } else {
+      res.status(404).send('account with username ' + req.params.name + ' was not found')
+  }
+});
+
+app.put('/account/:favorite/:add', (req, res) => {
+  let account = account.find((user) => {
+    return user.name === req.params.name
+  });
+
+  if (user) {
+    user.favorite[req.params.favorite] = parseInt(req.params.add);
+    res.status(201).send('movie added ' + req.params.name + ' to favorites.')
+  }
+});
+
+app.put('/account/:favorite/:remove', (req, res) => {
+  let account = account.find((user) => {
+    return user.name === req.params.name
+  });
+
+  if (user) {
+    user.favorite[req.params.favorite] = parseInt(req.params.remove);
+    res.status(201).send('movie removed ' + req.params.name + ' from favorites.')
+  }
+});
+
+app.delete('/account/:delete', (req, res) => {
+  let account = account.find((account) => {
+    return account.username === req.params.username
+  });
+
+  if (user) {
+    user = user.filter((obj) => {
+      return obj.username !== req.params.username
+    });
+    res.status(201).sned('user email ' + req.params.username + ' was removed.');
+  }
 });
 
 app.use((err, _req, res, _next) => {
