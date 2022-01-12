f73245ca6448ad43e4785f0d092be1a4d0e76318
nnajera95 <NathanNajera@yahoo.com>
2022-01-12 13:37:00 -0600

    Create auth.js
    if applied this commit will create the auth.js file as well as input code for the authentication process.

---
+++ auth.js
@@ -0,0 +1,26 @@
+const jwtSecret = 'your_jwt_secret';
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport');
+
+let generateJWTToken = (user) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = genereateJWTToken(user.toJSON());
+        return res.json({ user, token});
+      });
+    })(req, res);
+  });
+}
